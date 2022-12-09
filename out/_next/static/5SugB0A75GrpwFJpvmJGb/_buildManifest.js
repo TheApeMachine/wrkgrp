bf02279e441f07865039606431719a2a714bbc17
Daniel Owen van Dommelen <9572060+TheApeMachine@users.noreply.github.com>
2022-12-09 07:43:39 +0100

    export out

---
+++ out/_next/static/5SugB0A75GrpwFJpvmJGb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0bd1a5d12040ebd8.css","static/chunks/pages/index-b300725f18d4ff50.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
